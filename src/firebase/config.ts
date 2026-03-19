336c913bdd7867624fc021225b89852c0a93574b
BioLive <app.biolive@gmail.com>
2026-03-19 18:29:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6515136391-70288",
+  "appId": "1:390269185803:web:8b1bbc855c8ae924e57c0e",
+  "apiKey": "AIzaSyABpHc8hmLKWTSVA0qeZqKTQOjuUEO33lo",
+  "authDomain": "studio-6515136391-70288.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "390269185803"
+};
